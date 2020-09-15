9d0fc0c0bdf2ceeafa38d563b712e67fed3663c6
NoNamer777 <o.wellner@telfort.nl>
2020-09-15 13:38:19 +0200

    Initial commit

---
+++ src/test/java/nl/femalert/femserver/FemServerApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.femalert.femserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FemServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
