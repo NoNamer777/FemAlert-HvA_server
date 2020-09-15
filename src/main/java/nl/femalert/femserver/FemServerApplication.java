d51da7de32fb1fedba3310a67c2103be7addc2c3
NoNamer777 <o.wellner@telfort.nl>
2020-09-15 13:38:19 +0200

    Initial commit

---
+++ src/main/java/nl/femalert/femserver/FemServerApplication.java
@@ -0,0 +1,13 @@
+package nl.femalert.femserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FemServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FemServerApplication.class, args);
+	}
+
+}
